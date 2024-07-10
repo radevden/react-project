5d61911fa25fba3206c7344b5548db08e80d6078
radevden <radevden@gmail.com>
2024-07-10 22:02:38 +0200

    href

--- src/App.js
+++ src/App.js
@@ -14,6 +14,6 @@ export default function App() {
 
 function Footer(){
   return (<footer>
-    This project is coded by Denisa Radevová and is open-sourced on <a href="https://github.com/radevden/react-project.git" target="_blank" rel="noreferrer">Github</a> and hosted on <a href="https://brilliant-salmiakki-9a4626.netlify.app/ " target="_blank" rel="noreferrer">Netlify</a>
+    This project is coded by Denisa Radevová and is open-sourced on <a href="https://github.com/radevden/react-project.git" target="_blank" rel="noopener noreferrer">Github</a> and hosted on <a href="https://brilliant-salmiakki-9a4626.netlify.app/ " target="_blank" rel="noopener noreferrer">Netlify</a>
   </footer>);
 }
