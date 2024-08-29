7935b8df8e045275c6a7c9848f017ddcd74cab01
radevden <radevden@gmail.com>
2024-08-29 17:15:34 +0200

    return forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -24,7 +24,7 @@ if (loaded) {
                                 <ForecastDay data={dailyForecast}/>
                             </div>
                         );
-                        } 
+                        } else {return null}
                     })}
                 </div>
             </div>
